b9aa3f6d5000f73a71264798b4a9a2809030ec29
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2022-12-17 19:02:39 +0100

    Update Cita
    Implementación métodos CRUD usados
    
    Eliminación de un método inutil
    
    Creado un Constructor necesario
    
    Movidos los atributos de por consistencia con el diagrama

--- hospital/src/dominio/Cita.java
+++ hospital/src/dominio/Cita.java
@@ -1,48 +1,121 @@
 package dominio;
 
+import java.sql.SQLException;
+import java.text.DateFormat;
 import java.util.Date;
+import java.util.Vector;
+
+import persistencia.Agente;
 
 public class Cita {
-	private String[] diagnostico;
-	private Date fechaYHora;
-	private long duracion;
-	public Paciente tiene;
-	public Doctor realiza;
-
-	public Cita(String[] diagnostico, String fechaYHora, String duracion) {
-		throw new UnsupportedOperationException();
-	}
-
-	public void setDiagnostico(String diagnostico) {
-		throw new UnsupportedOperationException();
-	}
-
-	/**
-	 * return descripcion+fecha+horas+especialidad+pacienteCitado
-	 * // qu� informaci�n sobre el paciente citado le debemos dar?
-	 * @return 
+	/* En la base de datos se guardará:
+	 * fechaYhoraInicial; fechaYHoraFinal; diagnostico; DNIpaciente; DNIdoctor
 	 */
-	public String toString() {
-		throw new UnsupportedOperationException();
+	private Date fechaYHoraInicial;
+	private Date fechaYHoraFinal;
+	private String diagnostico;
+	public Paciente pacienteCitado;
+	public Doctor doctor;
+
+	public Cita(final Date fechaYHoraInicial, final Date fechaYHoraFinal, final String DNIpaciente, final Doctor doctor) throws Exception {
+		super();
+		this.fechaYHoraInicial = fechaYHoraInicial;
+		this.fechaYHoraFinal = fechaYHoraFinal;
+		this.pacienteCitado= (Paciente) Paciente.READ(DNIpaciente);
+		this.doctor= doctor;
+	}
+
+	public void setDiagnostico(final String diagnostico) {
+		this.diagnostico= diagnostico;
 	}
 
 	public boolean CREATE() {
 		throw new UnsupportedOperationException();
 	}
 
-	public Cita READ(String fechaYHora, String DNIPaciente) {
-		throw new UnsupportedOperationException();
+	public static Cita READ(final String DNIpaciente, final Doctor doctor) throws Exception {
+		final Agente a= Agente.getAgente();
+
+		final String columns[]= {
+				"DNIpaciente", // TODO: Usar constantes, pero dónde ponemos las constantes ???
+				"DNIdoctor",
+		};
+		final String values[]= {
+				DNIpaciente,
+				doctor.getDNI(),
+		};
+		// para poder pasar String con fechas a Date
+		final DateFormat d= DateFormat.getDateInstance();
+		
+		// leemos todas las citas asociadas a este doctor y este paciente
+		Vector<Vector<Object>> posiblesCitas= a.select("citas", columns, values);
+		
+		// obtenemos la fechaYHoraActual para poder saber qué cita de todas las posibles es la que se está llevando a cabo
+		final Date horaActual= new Date();
+
+		Cita citaActual= null;
+		for (int i= 0; i< posiblesCitas.size(); i++)
+			// si la hora actual es mayor que la de inicio de la cita (0)
+			if (horaActual.compareTo(d.parse((String) posiblesCitas.get(i).get(0)))== 0
+			// y si la hora actual es menor que la de finalización de la cita (1)
+			 && horaActual.compareTo(d.parse((String) posiblesCitas.get(i).get(1)))== 0)
+				// la cita i es la cita actual
+				citaActual= new Cita(
+						// hora de inicio TODO: ya se ha parseado previamente la hora inicial y final, esto se puede mejorar
+						d.parse((String) posiblesCitas.get(i).get(0)),
+						// hora final
+						d.parse((String) posiblesCitas.get(i).get(1)),
+						// DNI paciente
+						(String) posiblesCitas.get(i).get(3),
+						// doctor
+						doctor);
+
+		return citaActual;
 	}
 
-	public boolean UPDATE() {
-		throw new UnsupportedOperationException();
+	public boolean UPDATE() throws Exception {
+		final Agente a= Agente.getAgente();
+		
+		final String columnasACambiar[]= { "diagnostico", };
+		final String valoresACambiar[] = {  diagnostico , };
+
+		final String columnsIdentificativas[]= {
+				// recordemos que un paciente y un doctor pueden estar asignados a varias citas
+				"fechaYHoraInicial",
+				"DNIpaciente",
+				"DNIdoctor",
+		};
+		final String identificacion[]= {
+				fechaYHoraFinal.toString(),
+				pacienteCitado.getDNI(),
+				doctor.getDNI(),
+		};
+		
+		return a.update("citas", columnasACambiar, valoresACambiar, columnsIdentificativas, identificacion)== 1 ? true : false;
 	}
 
-	public boolean DELETE() {
-		throw new UnsupportedOperationException();
+	public boolean DELETE() throws SQLException, Exception {
+		final Agente a= Agente.getAgente();
+
+		final String columnsIdentificativas[]= {
+				// recordemos que un paciente y un doctor pueden estar asignados a varias citas
+				"fechaYHoraInicial",
+				"DNIpaciente",
+				"DNIdoctor",
+		};
+		final String identificacion[]= {
+				fechaYHoraFinal.toString(),
+				pacienteCitado.getDNI(),
+				doctor.getDNI(),
+		};
+		
+		return a.delete("citas", columnsIdentificativas, identificacion)== 1 ? true : false;
 	}
 
-	public void operation() {
-		throw new UnsupportedOperationException();
+	@Override
+	public String toString() {
+		return "Cita [fechaYHoraInicial=" + fechaYHoraInicial + ", fechaYHoraFinal=" + fechaYHoraFinal
+				+ ", diagnostico=" + diagnostico + ", pacienteCitado=" + pacienteCitado + ", doctor=" + doctor + "]";
 	}
+
 }
