9c0f74e22f2031e2028153fdc4be8c2b96e01d53
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2022-12-17 19:04:56 +0100

    Update Doctor
    Añadidos e implementados métodos diagnosticar y cancelarDiagnostico

--- hospital/src/dominio/Doctor.java
+++ hospital/src/dominio/Doctor.java
@@ -1,7 +1,7 @@
 package dominio;
 
+import java.sql.SQLException;
 import java.util.Vector;
-import dominio.Cita;
 
 /**
  * Debemos tener alg�n tipo de informaci�n para saber cu�ndo un doctor puede ser citado:
@@ -12,6 +12,40 @@ public class Doctor extends PersonalSanitario {
 	private String especialidad;
 	private String consulta;
 	public Vector<Cita> realiza = new Vector<Cita>();
+	
+	public Doctor() {
+		super();
+	}
+
+	public String diagnosticar(final String DNIpaciente, final String diagnostico) throws Exception {
+		try {
+			final Cita citaActual= Cita.READ(DNIpaciente, this);
+			if (citaActual== null) return "Error, no se ha podido encontrar la cita actual";
+
+			citaActual.setDiagnostico(diagnostico);
+			
+			if (citaActual.UPDATE())
+				return "Operación realizada satisfactoriamente";
+
+			return "Error al actualizar la cita.";
+		} catch (SQLException e) {
+			return "Ha ocurrido un error con la base de datos.";
+		}
+	}
+
+	public String cancelarDiagnostico(String DNIpaciente) throws Exception {
+		try {
+			final Cita citaActual= Cita.READ(DNIpaciente, this);
+			if (citaActual== null) return "Error, no se ha podido encontrar la cita actual";
+			
+			if (citaActual.DELETE()) 
+				return "Operación realizada satisfactoriamente";
+
+			return "Error al borrar la cita.";
+		} catch (SQLException e) {
+			return "Ha ocurrido un error con la base de datos.";
+		}
+	}
 
 	/**
 	 * Informaci�nADevolver
@@ -30,4 +64,5 @@ public class Doctor extends PersonalSanitario {
 	public boolean estoOcupadoEn(String fechayHora) {
 		throw new UnsupportedOperationException();
 	}
+
 }
