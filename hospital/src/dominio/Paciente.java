b37bd149c14ac4217c04a1b1480a13f945f386c1
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2022-12-18 17:35:43 +0100

    Actualización diagnosticar
    Se ha añadido un constructor en Paciente con el DNI
    
    Se ha sobrescrito el método READ de Persona en Paciente
    
    Se ha añadido la tabla de personas a la base de datos

--- hospital/src/dominio/Paciente.java
+++ hospital/src/dominio/Paciente.java
@@ -1,14 +1,34 @@
 package dominio;
 
+import java.util.Vector;
+
+import persistencia.Agente;
+
 public class Paciente extends Persona {
 	public enum PrioridadPaciente { VITAL, };
 	private PrioridadPaciente prioridad;
 	private String historialClinico;
 	public Ingreso esta;
+	
+	public Paciente(final String DNI) {
+		super(DNI);
+	}
 
 	public Paciente(PrioridadPaciente prioridad, String historial) {
 		super("DNI");
 		throw new UnsupportedOperationException();
+	}
+
+	public static Persona READ(String DNI) throws Exception {
+		final Agente a= Agente.getAgente();
+		final String columnas[]= { "DNI", };
+		final String valores[]= { DNI, };
+
+		Vector<Vector<Object>> posiblePaciente= a.select("pacientes", columnas, valores);
+		
+		if (posiblePaciente.size()!= 1) throw new NullPointerException("No se ha encontrado el trabajador buscado.");
+		
+		return new Paciente((String) posiblePaciente.get(0).get(0));
 	}
 
 	public String actualizarDatosAdministrativos(String[] info) {
